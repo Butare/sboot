0e26a952b7fb63828840b6e3280f9d2aa9a77199
Butare <bjimsons@gmail.com>
2023-09-24 16:18:57 +0900

    sort the ordered items

--- src/main/java/com/testnew/sboot/OrderController.java
+++ src/main/java/com/testnew/sboot/OrderController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
@@ -18,6 +20,7 @@ public class OrderController {
 
     @GetMapping
     public List<Order> getOrder() {
-        return orderService.getOrders();
+        return orderService.getOrders()
+                .stream().sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
     }
 }
